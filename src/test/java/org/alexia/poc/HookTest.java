e75772d3522b017277c117ac67398482b6119f0a
HOANG Alexia (Ext) <ahoang.ext@galerieslafayette.com>
2016-01-22 17:12:08 +0100

    POC: pre push fail test

--- src/test/java/org/alexia/poc/HookTest.java
+++ src/test/java/org/alexia/poc/HookTest.java
@@ -11,4 +11,11 @@ public class HookTest {
         int result = hook.addOne();
         assertThat(result).isEqualTo(3);
     }
+
+    @Test
+    public void should_add_failed() throws Exception {
+        Hook hook = new Hook(3);
+        int result = hook.addOne();
+        assertThat(result).isEqualTo(3);
+    }
 }
